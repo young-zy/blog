890f5ddf7ca8f2b2482262c98c16667bde06bd1a
young-zy <youngzhaozhou@gmail.com>
2021-02-23 22:08:52 +0800

    add api rate restriction add user field custom validation

--- go.mod
+++ go.mod
@@ -5,17 +5,16 @@ go 1.16
 require (
 	github.com/appleboy/gin-jwt/v2 v2.6.4
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-playground/validator/v10 v10.4.1
+	github.com/go-redis/redis/v8 v8.6.0 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/ugorji/go v1.2.0 // indirect
+	github.com/young-zy/gin-api-restriction v0.0.0-20210223055131-c3ebf35b14c2 // indirect
 	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.11
 )
--- models/user.go
+++ models/user.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
 type User struct {
 	Id             int    `gorm:"type:INT;NOT NULL" json:"id"`
 	Username       string `gorm:"type:VARCHAR(45);NOT NULL" json:"username"`
@@ -8,12 +14,44 @@ type User struct {
 }
 
 type LoginRequest struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Username string `json:"username" binding:"required,usernameRegex"`
+	Password string `json:"password" binding:"required,passwordRegex"`
 }
 
 type UserRegister struct {
-	Username string `json:"username" binding:"required"`
+	Username string `json:"username" binding:"required,usernameRegex"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required"`
+	Password string `json:"password" binding:"required,passwordRegex"`
+}
+
+type UserUpdate struct {
+	Username    string `json:"username" binding:"required"`
+	Email       string `json:"email" binding:"omitempty,email"`
+	Password    string `json:"password" binding:"required,passwordRegex"`
+	NewPassword string `json:"newPassword" binding:"omitempty,passwordRegex"`
+}
+
+func init() {
+	validate := validator.New()
+	_ = validate.RegisterValidation("usernameRegex", usernameRegex)
+	_ = validate.RegisterValidation("passwordRegex", passwordRegex)
+}
+
+func usernameRegex(fl validator.FieldLevel) bool {
+	reg := regexp.MustCompile("^(?=.{4,20}$)(?![_0-9])(?!.*[_]{2})[a-zA-Z0-9_-]+(?<![_])$")
+	username := fl.Field().String()
+	if reg.MatchString(username) {
+		return true
+	}
+	return false
+}
+
+func passwordRegex(fl validator.FieldLevel) bool {
+	// contains at least one capitalized, one uncapitalized, one number, length at least 8 but no more than 32
+	reg := regexp.MustCompile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z!@#$%^&*\\d]{8,32}$")
+	pass := fl.Field().String()
+	if reg.MatchString(pass) {
+		return true
+	}
+	return false
 }
