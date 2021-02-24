36614a35b5fe9dfbaa573778e9170cc13aae9b1c
young-zy <youngzhaozhou@gmail.com>
2021-02-24 18:14:53 +0800

    remove unused code

--- controllers/router.go
+++ controllers/router.go
@@ -31,30 +31,22 @@ func init() {
 	initUserGroup()
 }
 
-// Deprecated:
-func handleError(c *gin.Context, httpError common.HttpError) {
-	c.AbortWithStatusJSON(httpError.Status(), gin.H{
-		"code":    httpError.Status(),
-		"message": httpError.Message(),
-	})
-}
-
-func cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
-
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-		}
-
-		c.Next()
-	}
-}
+//func cors() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		method := c.Request.Method
+//		c.Header("Access-Control-Allow-Origin", "*")
+//		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+//		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+//		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+//		c.Header("Access-Control-Allow-Credentials", "true")
+//
+//		if method == "OPTIONS" {
+//			c.AbortWithStatus(http.StatusNoContent)
+//		}
+//
+//		c.Next()
+//	}
+//}
 
 func logging() gin.HandlerFunc {
 	return func(context *gin.Context) {
--- controllers/traceGenerator.go
+++
@@ -1,8 +0,0 @@
-package controllers
-
-import "github.com/gin-gonic/gin"
-
-func generateTraceId(c *gin.Context) {
-	//c.Set("traceId", "test")
-	c.Next()
-}
